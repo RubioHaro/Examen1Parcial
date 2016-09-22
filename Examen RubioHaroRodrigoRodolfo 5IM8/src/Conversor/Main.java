6426f108bd45ccf489ef2823405700ea21981ec8
RubioHaro <chavo0022009@gmail.com>
2016-09-22 14:35:54 -0500

    Add files via upload

---
+++ Examen RubioHaroRodrigoRodolfo 5IM8/src/Conversor/Main.java
@@ -0,0 +1,14 @@
+
+package Conversor;
+
+/**
+ * Ejercicio: 
+ * - Conversor
+ *@author:  RubioHaro
+ */
+public class Main {
+ public static void main(String[] args) {
+		Conversor calculadora = new Conversor();
+		calculadora.setVisible(true);
+	}   
+}
